d1a1db820823d0988475203c15a71acfec043530
kerBamf <zach.pedigo@gmail.com>
2023-04-11 21:02:29 -0400

    Finished intrument model and linking successfully to database

---
+++ models/instruments.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const instrumentsSchema = new mongoose.Schema(
+    {
+        name: {
+            required: [true, "All Fields Required"],
+            type: String,
+        },
+        brand: {
+            required: [true, "All Fields Required"],
+            type: String,
+        },
+        image: {
+            type: String,
+            default: "."
+        },
+        price: {
+            required: [true, "All Fields Required"],
+            type: String,
+        },
+        description: {
+            required: [true, "All Fields Required"],
+            type: String,
+        },
+        
+    }, {timestamps: true}
+)
+
+const Instruments = mongoose.model('instruments', instrumentsSchema)
+
+module.exports = Instruments
